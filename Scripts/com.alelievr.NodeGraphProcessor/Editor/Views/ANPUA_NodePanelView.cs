88769d1f4392fa8bf29d491744a723b761c6de3b
Christian Schnoor <>
2024-11-15 04:01:55 +0100

    Adding SidePanel Features
    -Fixed Layout
    -Added Button to Show Name
    -Adden Gray Backgrounds
    -Adjusted Font Size
    -Added Drag and Drop Functionality

--- Scripts/com.alelievr.NodeGraphProcessor/Editor/Views/ANPUA_NodePanelView.cs
+++ Scripts/com.alelievr.NodeGraphProcessor/Editor/Views/ANPUA_NodePanelView.cs
@@ -12,7 +12,7 @@ using System.Linq;
 namespace GraphProcessor
 {
 
-	
+
 	public class ANPUA_NodePanelView : PinnedElementView
 	{
 		BaseGraphProcessor processor;
@@ -20,6 +20,8 @@ namespace GraphProcessor
 		//Nodelist
 		IEnumerable<(string path, System.Type type)> nodeList;
 		BaseGraphView graphView;
+
+		private bool showNames = false;
 
 		//Dict for getting icons NodeType to get the Path Text
 		private Dictionary<Type, string> nodeTypeToPath = new Dictionary<Type, string>(){
@@ -33,6 +35,7 @@ namespace GraphProcessor
 		public ANPUA_NodePanelView()
 		{
 			title = "NodePanel";
+			scrollable = true;
 		}
 
 		protected override void Initialize(BaseGraphView graphView)
@@ -43,8 +46,21 @@ namespace GraphProcessor
 
 			graphView.computeOrderUpdated += processor.UpdateComputeOrder;
 
-			//Button b = new Button(OnPlay) { name = "ActionButton", text = "Play !" };
-			//content.Add(b);
+			Button b = new Button(OnHelp)
+			{
+				text = "",
+				style =
+						{
+							width = 17,
+							height = 17,
+								  backgroundImage = new StyleBackground((Texture2D)EditorGUIUtility.IconContent( "d__Help@2x").image),
+							paddingBottom = 5,
+							marginBottom = 5,
+						}
+
+			};
+			header.Add(b);
+
 
 			LoadNodes();
 			GenerateUI();
@@ -58,6 +74,7 @@ namespace GraphProcessor
 			nodeList = NodeProvider.GetNodeMenuEntries();
 		}
 
+		// Generate UI
 		// Generate UI
 		public void GenerateUI()
 		{
@@ -76,179 +93,220 @@ namespace GraphProcessor
 		}
 
 		// Recursive method to add nodes to the hierarchical structure
-        private void AddNodeToHierarchy(Dictionary<string, object> hierarchy, string[] parts, int index, (string path, Type type) node)
-        {
-            if (index >= parts.Length)
-                return;
+		private void AddNodeToHierarchy(Dictionary<string, object> hierarchy, string[] parts, int index, (string path, Type type) node)
+		{
+			if (index >= parts.Length)
+				return;
 
-            if (!hierarchy.ContainsKey(parts[index]))
-            {
-                if (index == parts.Length - 1)
-                {
-                    hierarchy[parts[index]] = node;
-                }
-                else
-                {
-                    hierarchy[parts[index]] = new Dictionary<string, object>();
-                }
-            }
+			if (!hierarchy.ContainsKey(parts[index]))
+			{
+				if (index == parts.Length - 1)
+				{
+					hierarchy[parts[index]] = node;
+				}
+				else
+				{
+					hierarchy[parts[index]] = new Dictionary<string, object>();
+				}
+			}
 
-            if (hierarchy[parts[index]] is Dictionary<string, object> subHierarchy)
-            {
-                AddNodeToHierarchy(subHierarchy, parts, index + 1, node);
-            }
-        }
-
-		// Recursive method to generate UI
-		// Recursive method to generate UI
-
-		public string getIconPath(Type type) {
-			//use dictionary to get the path
-			//check if the type is in the dictionary
-			if (nodeTypeToPath.ContainsKey(type)) 
-				return nodeTypeToPath[type];
-			// unity icon
-			return "d__Help";
+			if (hierarchy[parts[index]] is Dictionary<string, object> subHierarchy)
+			{
+				AddNodeToHierarchy(subHierarchy, parts, index + 1, node);
+			}
 		}
-	
 
-        private void GenerateUIRecursive(Dictionary<string, object> hierarchy, VisualElement parent, int level)
-        {
-            foreach (var kvp in hierarchy)
-            {
-                 if (kvp.Value is ValueTuple<string, Type> node)
-                {
-
+		// Recursive method to generate UI
+		private void GenerateUIRecursive(Dictionary<string, object> hierarchy, VisualElement parent, int level)
+		{
+			foreach (var kvp in hierarchy)
+			{
+				if (kvp.Value is ValueTuple<string, Type> node)
+				{
 					string path = node.Item1;
 					//remove path
 					string[] parts = path.Split('/');
 					string name = parts[parts.Length - 1];
-					
-                    // Create a button for the node
-                    Label nodeButton = new Label(
-                    )
-                    {
-                        text = "",
 
-						//Display name over hover without the path, but just the name like "name" not "path/name"
-						tooltip = name,
+			
+					Label nodeButton;
+					if (showNames)
+					{
+						nodeButton = new Label()
+						{
+							text = name,
+							tooltip = name,
+							style =
+						{
+							marginBottom = 3,
+							paddingLeft = 3,
+							paddingRight = 3,
+							paddingTop = 3,
 
-                        style =
-                        {
-                            width = 40,
-                            height = 40,
-                            backgroundImage = new StyleBackground((Texture2D)EditorGUIUtility.IconContent( getIconPath(node.Item2)).image),
-                        }
-                    };
-
-                    // Add drag-and-drop handlers
-                    nodeButton.RegisterCallback<MouseDownEvent>(evt => OnMouseDown(evt, node.Item2));
-                    nodeButton.RegisterCallback<MouseMoveEvent>(OnMouseMove); 
-                    nodeButton.RegisterCallback<MouseUpEvent>(OnMouseUp);
-
-                    // Add the node button to the current row container
-                    AddToRowContainer(parent, nodeButton);
-                }
-                else if (kvp.Value is Dictionary<string, object> subHierarchy)
-                {
-                    // Add a title for the group or subgroup
-                    var title = new Label(kvp.Key)
-                    {
-                        style =
-                        {
-                            unityFontStyleAndWeight = FontStyle.Bold,
-                            fontSize = level == 0 ? 16 : 14,
-                            marginBottom = 2,
-							flexWrap = Wrap.Wrap
-                        }
-                    };
-                    parent.Add(title);
-
-                    // Create a new container for the subgroup
-                    var subgroupContainer = new VisualElement
-                    { 
-                        style =
-                        {
-                            flexDirection = FlexDirection.Column,
-                            marginBottom = 10
-                        }
-                    };
-                    parent.Add(subgroupContainer);
-
-                    // Recursively generate UI for the subgroup
-                    GenerateUIRecursive(subHierarchy, subgroupContainer, level + 1);
-                }
-            }
-        }
-		// Method to add elements to row containers
+						}
+						};
+					}
+					else
+					{
+						nodeButton = new Label()
+						{
+							text = "",
+							tooltip = name,
 
 
- // Method to add elements to row containers
-        private void AddToRowContainer(VisualElement parent, VisualElement element)
-        {
-            // Get the last row container or create a new one if necessary
-            VisualElement rowContainer = parent.Children().LastOrDefault() as VisualElement;
-            if (rowContainer == null || rowContainer.childCount >= 5)
-            {
-                rowContainer = new VisualElement
-                {
-                    style =
-                    {
-                        flexDirection = FlexDirection.Row,
-						flexWrap = Wrap.Wrap,
-                        marginBottom = 5
-                    }
-                };
-                parent.Add(rowContainer);
-            }
+							style =
+						{
+							width = 25,
+							height = 25,
+								  backgroundImage = new StyleBackground((Texture2D)EditorGUIUtility.IconContent( getIconPath(node.Item2)).image),
+						}
+						};
+					}
 
-            // Add the element to the row container
-            rowContainer.Add(element);
-        }
 
-        // Drag-and-drop handlers
-        private void OnMouseDown(MouseDownEvent evt, Type nodeType)
-        {
-            DragAndDrop.PrepareStartDrag();
-            DragAndDrop.SetGenericData("NodeType", nodeType);
-            DragAndDrop.StartDrag("Dragging Node");
-            evt.StopPropagation();
-        }
 
-        private void OnMouseMove(MouseMoveEvent evt)
-        {
-            if (DragAndDrop.GetGenericData("NodeType") != null)
-            {
-                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
-                evt.StopPropagation();
-            }
-        }
+					// Add the node button to the current row container
+					if (showNames)
+					{
 
-        private void OnMouseUp(MouseUpEvent evt)
-        {
-            if (DragAndDrop.GetGenericData("NodeType") != null)
-            {
-                var nodeType = DragAndDrop.GetGenericData("NodeType") as Type;
-                var mousePosition = evt.mousePosition;
-                var graphPosition = graphView.contentViewContainer.WorldToLocal(mousePosition);
+						AddToRowContainer(parent, nodeButton);
+					}
+					else
+					{
+						parent.Add(nodeButton);
+					}
+				}
+				else if (kvp.Value is Dictionary<string, object> subHierarchy)
+				{
+					// Create a box container for the group or subgroup
+					var box = new Box
+					{
+						style =
+						{
+							marginBottom = 3,
+							paddingLeft = 3,
+							paddingRight = 3,
+							paddingTop = 3,
+							paddingBottom = 3,
+							borderLeftWidth = 1,
+							borderRightWidth = 1,
+							borderTopWidth = 1,
+							borderBottomWidth = 1,
+							//BackgroundColor
+							backgroundColor = new Color(0.02f, 0.02f, 0.02f, 0.25f)
 
-				var center = graphView.contentViewContainer.layout.center;
+						}
+					};
 
-                // Check if the release position is within the graph's window
-                if (graphView.contentViewContainer.ContainsPoint(graphPosition))
-                {
-                    graphView.AddNode(BaseNode.CreateFromType(nodeType, center));
-                }
+					// Add a title for the group or subgroup
+					var title = new Label(kvp.Key)
+					{
+						style =
+						{
+							unityFontStyleAndWeight = FontStyle.Bold,
+							fontSize = level == 0 ? 14 : 10,
+							marginBottom = 10
+						}
+					};
+					box.Add(title);
 
-                DragAndDrop.AcceptDrag();
-                DragAndDrop.SetGenericData("NodeType", null);
-                evt.StopPropagation();
-            }
-        }
+					// Create a new container for the subgroup
+					var subgroupContainer = new VisualElement
+					{
+						style =
+						{
+							flexDirection = FlexDirection.Row,
+							flexWrap = Wrap.Wrap,
+							marginBottom = 5
+						}
+					};
+					box.Add(subgroupContainer);
 
-		void OnPlay()
+					// Add the box to the parent container
+					parent.Add(box);
+
+					// Recursively generate UI for the subgroup
+					GenerateUIRecursive(subHierarchy, subgroupContainer, level + 1);
+				}
+			}
+		}
+		public string getIconPath(Type type)
 		{
-			processor.Run();
+			//use dictionary to get the path
+			//check if the type is in the dictionary
+			if (nodeTypeToPath.ContainsKey(type))
+				return nodeTypeToPath[type];
+			// unity icon
+			return "d__Help@2x";
+		}
+
+		// Method to add elements to row containers
+		private void AddToRowContainer(VisualElement parent, VisualElement element)
+		{
+			// Get the last row container or create a new one if necessary
+			VisualElement rowContainer = parent.Children().LastOrDefault() as VisualElement;
+			if (rowContainer == null || rowContainer.childCount >= 5)
+			{
+				rowContainer = new VisualElement
+				{
+					style =
+					{
+						flexDirection = FlexDirection.Row,
+						marginBottom = 5
+					}
+				};
+				parent.Add(rowContainer);
+			}
+
+			// Add the element to the row container
+			rowContainer.Add(element);
+		}
+
+		// Drag-and-drop handlers
+		private void OnMouseDown(MouseDownEvent evt, Type nodeType)
+		{
+			DragAndDrop.PrepareStartDrag();
+			DragAndDrop.SetGenericData("NodeType", nodeType);
+			DragAndDrop.StartDrag("Dragging Node");
+			evt.StopPropagation();
+		}
+
+		private void OnMouseMove(MouseMoveEvent evt)
+		{
+			if (DragAndDrop.GetGenericData("NodeType") != null)
+			{
+				DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+				evt.StopPropagation();
+			}
+		}
+
+		private void OnMouseUp(MouseUpEvent evt)
+		{
+			if (DragAndDrop.GetGenericData("NodeType") != null)
+			{
+				var nodeType = DragAndDrop.GetGenericData("NodeType") as Type;
+				var mousePosition = evt.mousePosition;
+				var graphPosition = graphView.contentViewContainer.WorldToLocal(mousePosition);
+
+				// Check if the release position is within the graph's window
+				if (graphView.contentViewContainer.ContainsPoint(graphPosition))
+				{
+					Debug.Log($"Node type: {nodeType}, Release position: {graphPosition}");
+				}
+
+				DragAndDrop.AcceptDrag();
+				DragAndDrop.SetGenericData("NodeType", null);
+				evt.StopPropagation();
+			}
+		}
+
+		void OnHelp()
+		{
+			showNames = !showNames;
+			//Reload the UI
+			content.Clear();
+			GenerateUI();
+
 		}
 	}
 }
